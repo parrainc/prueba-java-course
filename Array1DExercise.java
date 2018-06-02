c1af237a9eb583e1c0f6de8da634ebbceb952431
Carlos Parra <carlos.parar.sbr@gmail.com>
2018-06-02 14:09:08 -0400

    Added solution for 1D array exercise

---
+++ Array1DExercise.java
@@ -0,0 +1,17 @@
+public class Array1DExercise {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        int val = 0;
+
+        for (int index : n) {
+        	a[x] = val = scan.nextInt();
+        }
+        scan.close();
+
+        for (int i = 0; i < a.length; i++) {
+        	System.out.println(a[i]);
+        }
+	}
+}
